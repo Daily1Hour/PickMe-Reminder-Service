2a8d7bdd30ec3887749398b6b01ad1d39bb55a7d
NarciSource <jwch11@gmail.com>
2024-12-03 18:04:29 +0900

    feat: NestJS 모듈 추가

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from "@nestjs/core";
 
+import { NotificationModule } from "./notification/notification.module";
+
 async function bootstrap() {
-    const app = await NestFactory.create(null);
+    const app = await NestFactory.create(NotificationModule);
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
