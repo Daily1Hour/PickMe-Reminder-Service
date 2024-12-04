e20770009093a1462e42b7fd6d689f1c8699fc95
NarciSource <jwch11@gmail.com>
2024-12-04 23:01:10 +0900

    feat: Swagger 배포 추가

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 import { NotificationModule } from "./module";
 import { JwtInterceptor } from "./infrastructure/auth/jwtInterceptor";
 
 async function bootstrap() {
     const app = await NestFactory.create(NotificationModule);
+
+    // Swagger
+    const swagger_config = new DocumentBuilder()
+        .setTitle("알림 API")
+        .setDescription("API 명세서") // API 설명
+        .setVersion("1.0")
+        .build();
+
+    // Swagger 설정
+    const document = SwaggerModule.createDocument(app, swagger_config);
+    // Swagger UI 설정
+    SwaggerModule.setup("api", app, document);
 
     // 전역 파이프 설정
     app.useGlobalPipes(
