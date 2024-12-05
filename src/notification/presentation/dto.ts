98c159d3f9393d22832988e25ecc7fcedf5b26e4
NarciSource <jwch11@gmail.com>
2024-12-05 11:42:33 +0900

    feat: TrimSeconds 데코레이터 추가

--- src/notification/presentation/dto.ts
+++ src/notification/presentation/dto.ts
@@ -3,13 +3,15 @@ import { Type } from "class-transformer";
 import { IsString, IsDate, IsOptional } from "class-validator";
 
 import { NotificationStatus } from "src/notification/application/dto";
-
+import { TrimSeconds } from "./TrimSeconds";
+  
 export class CreateRequestDTO {
     @IsString()
     @ApiProperty({ description: "수행될 이벤트 ID", example: "507f1f77bcf86cd799439011" })
     event_id: string;
 
     @IsDate()
+    @TrimSeconds() 
     @Type(() => Date)
     @ApiProperty({ description: "알림 발송 시간" })
     send_at: Date;
