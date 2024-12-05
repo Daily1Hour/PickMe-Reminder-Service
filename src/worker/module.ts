f69cffd25864ecab41999206644acb2ed32b6058
NarciSource <jwch11@gmail.com>
2024-12-05 20:58:51 +0900

    feat: 크론 작업 설정

--- src/worker/module.ts
+++ src/worker/module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
+import { ScheduleModule } from "@nestjs/schedule";
 
 import { NotificationModule } from "../notification/module";
 
 import { WorkerService } from "./service";
 
 @Module({
-    imports: [NotificationModule],
+    imports: [ScheduleModule.forRoot(), NotificationModule],
     providers: [WorkerService],
 })
 export class WorkerModule {}
