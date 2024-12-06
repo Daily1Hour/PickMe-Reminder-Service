f6b690c38eb1979b1eaa86954478e777b083c117
NarciSource <jwch11@gmail.com>
2024-12-06 10:47:07 +0900

    feat: 알림 부분 수정 메시지 기반 API 추가

--- src/notification/presentation/controllers/messageController.ts
+++ src/notification/presentation/controllers/messageController.ts
@@ -1,10 +1,10 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Patch } from "@nestjs/common";
 import { ApiOperation, ApiTags, ApiBody } from "@nestjs/swagger";
 import { MessagePattern, Payload } from "@nestjs/microservices";
 
 import NotificationService from "src/notification/application/service";
 
-import { ParametersDTO } from "../dtos";
+import { ParametersDTO, UpdateRequestDTO } from "../dtos";
 
 @Controller("message")
 @ApiTags("TCP API")
@@ -20,5 +20,15 @@ export default class NotificationsMessageController {
     @MessagePattern({ cmd: "readByOptions" })
     async readByOptions(@Payload() payload: ParametersDTO) {
         return this.service.getFilteredList(payload);
+    }
+
+    @Patch("docs")
+    @ApiOperation({
+        summary: "알림 부분 수정",
+        description: "이 API는 메시지 브로커를 통해 동작하며, HTTP 요청으로는 사용되지 않습니다.",
+    })
+    @MessagePattern({ cmd: "updatePartial" })
+    async updatePartial(@Payload() { event_id, ...args }: UpdateRequestDTO) {
+        return this.service.update({ event_id }, args);
     }
 }
