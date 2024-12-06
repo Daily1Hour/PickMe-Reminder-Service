9bd6b18eaf881d7e796d3afd6f51ebbc8baf1d40
NarciSource <jwch11@gmail.com>
2024-12-06 17:00:23 +0900

    feat: 알림 발송 처리 중 에러 핸들링 추가

--- src/worker/application/service.ts
+++ src/worker/application/service.ts
@@ -12,23 +12,27 @@ export class WorkerService {
         const start_time = new Date();
         const end_time = new Date(start_time.getTime() + 60 * 60 * 1000); // 1시간 후
 
-        // 발송할 알림들
-        const notifications = await this.client.readByOptions({
-            start_time,
-            end_time,
-            status: NotificationStatus.Pending,
-        });
-
-        // 발송 처리
-        await dispatch(notifications);
-
-        console.log("발송 완료 처리");
-        // 발송 완료 처리
-        for (const notification of notifications) {
-            await this.client.updatePartial({
-                event_id: notification.event_id,
-                status: NotificationStatus.Sent,
+        try {
+            // 발송할 알림들
+            const notifications = await this.client.readByOptions({
+                start_time,
+                end_time,
+                status: NotificationStatus.Pending,
             });
+
+            // 발송 처리
+            await dispatch(notifications);
+
+            console.log("발송 완료 처리");
+            // 발송 완료 처리
+            for (const notification of notifications) {
+                await this.client.updatePartial({
+                    event_id: notification.event_id,
+                    status: NotificationStatus.Sent,
+                });
+            }
+        } catch (error) {
+            console.error("발송 처리 중 에러 발생", error);
         }
     }
 }
