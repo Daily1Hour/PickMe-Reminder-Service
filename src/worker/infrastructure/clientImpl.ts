e3ac8cd19c0ae789a41a3b81a4bf03b5590fdb48
NarciSource <jwch11@gmail.com>
2024-12-07 14:16:05 +0900

    refactor: TCP 연결 모듈간 의존성 끊기

--- src/worker/infrastructure/clientImpl.ts
+++ src/worker/infrastructure/clientImpl.ts
@@ -1,15 +1,24 @@
 import { Injectable } from "@nestjs/common";
-import { Client, ClientTCP, Transport } from "@nestjs/microservices";
+import { ClientTCP } from "@nestjs/microservices";
 import { firstValueFrom } from "rxjs";
 
 import { IWorkerClient } from "../application/client";
 
 @Injectable()
 export class WorkerClientImpl implements IWorkerClient {
-    // 마이크로서비스로 TCP 연결
-    @Client({ transport: Transport.TCP, options: { host: "localhost", port: 3001 } })
     private client: ClientTCP;
 
+    constructor() {
+        // 마이크로서비스 TCP 연결
+        this.client = new ClientTCP({
+            host: "localhost",
+            port: process.env.MS_PORT || 3001, // Notification 서비스의 포트
+        });
+    }
+
+    async onModuleInit() {
+        await this.client.connect();
+    }
     async readByOptions(query: any) {
         // 마이크로서비스로 요청
         return firstValueFrom(this.client.send({ cmd: "readByOptions" }, query));
