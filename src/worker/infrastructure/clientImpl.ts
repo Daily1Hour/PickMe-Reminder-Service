897e647a389fd9224edcf690830e99ee5d08a71a
NarciSource <jwch11@gmail.com>
2024-12-07 17:55:03 +0900

    chore: Docker Compose로 서비스 연결

--- src/worker/infrastructure/clientImpl.ts
+++ src/worker/infrastructure/clientImpl.ts
@@ -10,6 +10,17 @@ export class WorkerClientImpl implements IWorkerClient {
     @Client({ transport: Transport.TCP, options: { host: "localhost", port: 3001 } })
     private client: ClientTCP;
 
+    constructor() {
+        // 마이크로서비스 TCP 연결
+        this.client = new ClientTCP({
+            host: process.env.MS_HOST || "localhost", // Notification 서비스의 호스트
+            port: process.env.MS_PORT || 3001, // Notification 서비스의 포트
+        });
+    }
+
+    async onModuleInit() {
+        await this.client.connect();
+    }
     async readByOptions(query: any) {
         // 마이크로서비스로 요청
         return firstValueFrom(this.client.send({ cmd: "readByOptions" }, query));
