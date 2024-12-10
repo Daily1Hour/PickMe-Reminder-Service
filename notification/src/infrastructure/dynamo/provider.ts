4a4a242e915642bdcff17186a689657d82cdb6bd
NarciSource <jwch11@gmail.com>
2024-12-10 21:41:27 +0900

    chore: Docker Compose에서 DynamoDB로 데이터베이스 서비스 변경

--- notification/src/infrastructure/dynamo/provider.ts
+++ notification/src/infrastructure/dynamo/provider.ts
@@ -1,9 +1,12 @@
 import * as dynamoose from "dynamoose";
 
+const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_PORT = process.env.DB_PORT || "8000";
+
 export default {
     provide: "DYNAMOOSE",
     useFactory: () => {
-        dynamoose.aws.ddb.local("http://localhost:8000"); // 로컬 설정
+        dynamoose.aws.ddb.local(`${DB_HOST}:${DB_PORT}`); // 로컬 설정
         return dynamoose;
     },
 };
