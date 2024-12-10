192fe12c350f50592e019f87ef6b89becbec9656
NarciSource <jwch11@gmail.com>
2024-12-10 17:57:22 +0900

    feat: DynamoDB로 마이그레이션

---
+++ notification/src/infrastructure/DynamoRepository.ts
@@ -0,0 +1,39 @@
+import dynamoose from "dynamoose";
+import { Inject, Injectable } from "@nestjs/common";
+
+import NotificationEntity, { NotificationStatus } from "domain/entity";
+
+@Injectable()
+export default class DynamoRepository {
+    private readonly model;
+
+    constructor(@Inject("DYNAMOOSE") private readonly dynamooseInstance: typeof dynamoose) {
+        this.model = dynamooseInstance.model(
+            "PickMe-Reminder",
+            new dynamooseInstance.Schema({
+                event_id: { type: String, hashKey: true },
+                send_at: Date,
+                status: {
+                    type: String,
+                    enum: Object.values(NotificationStatus),
+                },
+            }),
+        );
+    }
+
+    async create(eventData: NotificationEntity) {
+        return this.model.create(eventData);
+    }
+
+    async findById(event_id: string) {
+        return this.model.get(event_id);
+    }
+
+    async findByReservationTime(send_at: Date) {
+        return this.model.query("send_at").eq(send_at).exec();
+    }
+
+    async deleteById(event_id: string) {
+        return this.model.delete(event_id);
+    }
+}
