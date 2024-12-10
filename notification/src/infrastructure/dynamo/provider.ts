e8c85b67787469ac3100e633c44ee3fb16650e4e
NarciSource <jwch11@gmail.com>
2024-12-10 22:51:38 +0900

    fix: DynamoDB 로컬 엔드포인트에 자격 증명 추가
    - Region Missing Error 발생 → 로컬에서도 자격 증명이 필요 → Fake 자격 증명 추가

--- notification/src/infrastructure/dynamo/provider.ts
+++ notification/src/infrastructure/dynamo/provider.ts
@@ -1,12 +1,21 @@
 import * as dynamoose from "dynamoose";
 
-const DB_HOST = process.env.DB_HOST || "localhost";
-const DB_PORT = process.env.DB_PORT || "8000";
-
 export default {
     provide: "DYNAMOOSE",
     useFactory: () => {
-        dynamoose.aws.ddb.local(`${DB_HOST}:${DB_PORT}`); // 로컬 설정
+        const DB_HOST = process.env.DB_HOST || "localhost";
+        const DB_PORT = process.env.DB_PORT || "8000";
+
+        const ddb = new dynamoose.aws.ddb.DynamoDB({
+            credentials: {
+                accessKeyId: "fakeKey",
+                secretAccessKey: "fakeSecretKey",
+            },
+            endpoint: `http://${DB_HOST}:${DB_PORT}`,
+            region: "local", // 로컬 설정
+        });
+        dynamoose.aws.ddb.set(ddb);
+
         return dynamoose;
     },
 };
