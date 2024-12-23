ecdedaafcc825af57b3ef84a93d11cb0b8f7f37a
NarciSource <jwch11@gmail.com>
2024-12-23 22:41:21 +0900

    fix: 알림 발송완료 반영 수정

--- notification/src/presentation/controllers/messageController.ts
+++ notification/src/presentation/controllers/messageController.ts
@@ -28,8 +28,10 @@ export default class NotificationsMessageController {
         summary: "알림 부분 수정",
         description: "이 API는 메시지 브로커를 통해 동작하며, HTTP 요청으로는 사용되지 않습니다.",
     })
+    @ApiBody({ type: UpdateRequestDTO })
     @MessagePattern({ cmd: "updatePartial" })
-    async updatePartial(@PayloadEX(ParametersDTO) { event_id, ...args }: UpdateRequestDTO) {
+    async updatePartial(@PayloadEX(UpdateRequestDTO) payload: UpdateRequestDTO) {
+        const { event_id, ...args } = payload;
         return this.service.update({ event_id }, args);
     }
 }
