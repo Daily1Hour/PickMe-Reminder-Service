a891fbbd7ce5ccb8689398910c7751fc6042bfed
NarciSource <jwch11@gmail.com>
2025-03-18 10:21:29 +0900

    test: Jest 설정 파일 추가 및 구성 업데이트

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,10 @@
-const { pathsToModuleNameMapper } = require("ts-jest");
-const { compilerOptions } = require("./tsconfig");
-
 module.exports = {
-    moduleFileExtensions: ["js", "json", "ts"],
-    rootDir: "./",
-    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/" }),
-    testRegex: ".*\\.spec\\.ts$",
-    transform: {
-        "^.+\\.(t|j)s$": "ts-jest",
-    },
-    collectCoverageFrom: ["**/*.(t|j)s"],
-    coverageDirectory: "../coverage",
+    projects: ["<rootDir>/notification", "<rootDir>/worker"],
+
+    rootDir: ".", // 루트 경로 지정
+    moduleFileExtensions: ["js", "json", "ts"], // jest가 해석할 파일 확장자 지정
+    testRegex: ".*\\.(spec|test)\\.ts$", // 테스트 파일 이름 지정
+    collectCoverageFrom: ["**/*.(t|j)s"], // 테스트 커버리지 측정 대상 파일 지정
+    coverageDirectory: "../coverage", // 커버리지 보고서
     testEnvironment: "node",
 };
