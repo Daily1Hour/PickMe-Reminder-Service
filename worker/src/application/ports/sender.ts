90dcff43066e7aea313c49c78a57565cafa3ac7f
NarciSource <jwch11@gmail.com>
2025-04-12 21:59:28 +0900

    docs: worker 서비스 TypeDoc 문서 정리

--- worker/src/application/ports/sender.ts
+++ worker/src/application/ports/sender.ts
@@ -1,5 +1,8 @@
 import { EventDetail } from "../dto";
 
+/**
+ * INotificationSender는 알림을 전송하기 위한 인터페이스입니다.
+ */
 export interface INotificationSender {
     dispatch(notification: EventDetail): Promise<void>;
 }
