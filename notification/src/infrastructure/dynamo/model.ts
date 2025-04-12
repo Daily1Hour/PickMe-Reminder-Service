dc564516a42c2dedd03be608de411420d43adcc6
NarciSource <jwch11@gmail.com>
2025-04-12 21:26:04 +0900

    docs: notification 서비스 TypeDoc 문서 정리

--- notification/src/infrastructure/dynamo/model.ts
+++ notification/src/infrastructure/dynamo/model.ts
@@ -5,10 +5,28 @@ import { Inject, Injectable } from "@nestjs/common";
 
 import { NotificationStatus } from "domain/entity";
 
+/**
+ * DynamooseModel 클래스는 DynamoDB와 상호작용하기 위한 Dynamoose 모델을 생성하고 관리합니다.
+ *
+ * @@Injectable 데코레이터를 사용하여 의존성 주입이 가능하도록 설정됩니다.
+ *
+ * @class
+ */
 @Injectable()
 export default class DynamooseModel {
+    /**
+     * Dynamoose 모델 인스턴스를 저장하는 필드입니다.
+     * @private
+     * @type {ModelType<AnyItem>}
+     */
     private readonly model: ModelType<AnyItem>;
 
+    /**
+     * DynamooseModel 클래스의 생성자입니다.
+     *
+     * @param {typeof dynamoose} instance - 의존성 주입된 Dynamoose 인스턴스입니다.
+     * @Inject("DYNAMOOSE") 데코레이터를 통해 주입됩니다.
+     */
     constructor(@Inject("DYNAMOOSE") private readonly instance: typeof dynamoose) {
         const schema = new instance.Schema({
             event_id: { type: String, hashKey: true },
@@ -29,7 +47,12 @@ export default class DynamooseModel {
         this.model = this.instance.model("PickMe-Reminder", schema);
     }
 
-    getModel() {
+    /**
+     * Dynamoose 모델 인스턴스를 반환합니다.
+     *
+     * @returns {ModelType<AnyItem>} Dynamoose 모델 인스턴스
+     */
+    getModel(): ModelType<AnyItem> {
         return this.model;
     }
 }
