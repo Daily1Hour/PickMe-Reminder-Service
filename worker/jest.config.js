13172b224c58761d63530d9671acec7546b1a95b
NarciSource <jwch11@gmail.com>
2025-04-13 20:03:35 +0900

    test: worker E2E 테스트 추가

--- worker/jest.config.js
+++ worker/jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
         "^infrastructure/(.*)$": "<rootDir>/src/infrastructure/$1",
         "^utility/(.*)$": "<rootDir>/src/utility/$1",
     },
+    testRegex: ".*\\.(e2e-spec|spec|test)\\.ts$", // 테스트 파일 이름 지정
     transform: {
         // ts-jest를 사용하여 테스트 파일을 해석
         "^.+\\.(t|j)s$": "ts-jest",
